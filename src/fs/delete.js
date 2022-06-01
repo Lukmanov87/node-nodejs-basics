ae3e8a52cb7b6b13a2731764ab496460743950f9
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-02 00:22:31 +0300

    feat: done delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import { unlink, readdir } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 export const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const pathToFolder = join(__dirname, "files");
+    const files = await readdir(pathToFolder);    
+
+    const fileName = "fileToRemove.txt";
+
+    if (!files.includes("fileToRemove.txt")) {
+        console.log("FS operation failed");
+      } 
+      else {
+        await unlink(join(pathToFolder, fileName));
+      }
 };
+remove();
