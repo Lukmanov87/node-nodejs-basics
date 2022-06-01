9fd76cee368ed3de05162d4bd0ee9676d8fb6b69
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-01 05:37:32 +0300

    feat: done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import { writeFile, readdir } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 export const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFolder = join(__dirname, "files");
+  const files = await readdir(pathToFolder);
+
+  const fileName = "fresh.txt";
+  const fileContent = "I am fresh and young";
+
+  if (files.includes("fresh.txt")) {
+    console.log("FS operation failed");
+  } else {
+    await writeFile(join(pathToFolder, fileName), fileContent);
+  }
 };
+create();
