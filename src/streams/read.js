6a837e7bca2e91b87262e685ec6792c4e6f8dd99
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-04 20:28:08 +0300

    feat: done streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import { createReadStream } from "fs";
+import { join } from "path";
+import { stdout } from "process";
+
 export const read = async () => {
-    // Write your code here 
+  const targetFileName = "fileToRead.txt";
+  const targetFolder = "src/streams/files/";
+  const targetFile = join(targetFolder, targetFileName);
+
+  const rs = createReadStream(targetFile);
+  rs.on("data", (data) => stdout.write(data));
 };
+// read();
