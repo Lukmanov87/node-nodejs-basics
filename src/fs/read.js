baa9d572a2c54ff5a7e347a567e3339211c881ab
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-04 19:09:32 +0300

    feat: done read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import { readdir, readFile } from "fs";
+
 export const read = async () => {
-    // Write your code here 
+  const targetFolder = "src/fs/files/";
+  const targetFile = "fileToRead.txt";
+  readdir(targetFolder, { withFileTypes: true }, (err) => {
+    if (err) throw err;
+    readFile(targetFolder + targetFile, "utf8", (err, data) => {
+      if (err) console.log("FS operation failed");
+      else {
+        console.log(data);
+      }
+    });
+  });
 };
+
+read();
