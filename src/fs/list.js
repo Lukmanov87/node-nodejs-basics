150a44a9882c6c3d303d46f1aa417c97c397cc1b
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-05 03:42:38 +0300

    feat: done fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import { readdir } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 export const list = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const targetFolder = join(__dirname, "files");
+    const files = await readdir(targetFolder);
+    for (const file of files) console.log(file);
+  } catch (err) {
+    console.log("FS operation failed");
+  }
 };
+list();
