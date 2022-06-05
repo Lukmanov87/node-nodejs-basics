9bcf7f7e55f2d8c527df7767f67c07943d27ac3f
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-05 03:14:26 +0300

    feat: done streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,24 @@
+import { createWriteStream } from "fs";
+import { join } from "path";
+import { stdin } from "process";
+
 export const write = async () => {
-    // Write your code here 
+  const targetFileName = "fileToWrite.txt";
+  const targetFolder = "src/streams/files/";
+  const output = createWriteStream(join(targetFolder, targetFileName));
+
+  stdin.on("data", (data) => {
+    //  Принудительное завершение при вводе ключевого слова **exit**
+    if (data.toString().trim() === "exit") {
+      process.exit();
+    }
+
+    output.write(data);
+  });
+
+  //  Принудительное завершение при нажатии `ctrl + c`
+  process.on("SIGINT", () => {
+    process.exit();
+  });
 };
+write();
