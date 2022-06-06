ba648c8eee809426f5a77e867ec8f28df66fc6c2
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-06 05:21:13 +0300

    feat: done streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,20 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
+class TranformStream extends Transform {
+  constructor(props = {}) {
+    super(props);
+  }
+
+  _transform(chunk, enc, cb) {
+    const chunkStringField = chunk.toString();
+    const reverseChunk = chunkStringField.split("").reverse().join("");    
+    cb(null, reverseChunk + "\n");
+  }
+}
+
 export const transform = async () => {
-    // Write your code here 
+  await pipeline(process.stdin, new TranformStream(), process.stdout);
 };
+
+await transform();
