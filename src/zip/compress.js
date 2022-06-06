8c87a63b2e9d60cce4973fd3629ae8be11d524d6
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-06-06 07:25:47 +0300

    feat: done zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createReadStream, createWriteStream, unlink } from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+
 export const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const targetReadFile = join(__dirname, "files", "fileToCompress.txt");
+  const targetWriteFile = join(__dirname, "files", "compressedFile.txt.gz");
+
+  const readableStream = createReadStream(targetReadFile);
+  const writableStream = createWriteStream(targetWriteFile);
+
+  await pipeline(readableStream, createGzip(), writableStream);
+
+  unlink(targetReadFile, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
+
+await compress();
