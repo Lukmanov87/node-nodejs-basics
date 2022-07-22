ceb7e6c0d3173861c5273ad107073d14273916dc
Lukmanov87 <61542812+Lukmanov87@users.noreply.github.com>
2022-07-22 21:36:18 +0300

    feat: done cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const userInputArgs = process.argv.slice(2);
+  const cliArgument = userInputArgs.reduce((acc, arg, index, arr) => {
+    const valueCandidate = arr[index + 1];
+    if (valueCandidate && arg.startsWith("--")) {
+      const transformArgs = arg.slice(2);
+      const cliArgumentTransformed = `${transformArgs} is ${valueCandidate}`;
+      acc.push(cliArgumentTransformed);
+    }
+    return acc;
+  }, []);
+  console.log(cliArgument.join(", "));
 };
+parseArgs();
